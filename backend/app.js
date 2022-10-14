93d9c9c0c3721156c18ba310887bbd02add0bbb2
estrellalvarez <alvare89@uwm.edu>
2022-10-14 11:58:38 -0500

    Backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from 'cors';
 
-app.use(cors());
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
@@ -16,6 +16,6 @@ app.use("/blog", blogRouter);
 mongoose.connect(
     'mongodb+srv://admin:pane@cluster0.wvqjpni.mongodb.net/?retryWrites=true&w=majority'
     )
-    .then(()=> app.listen(5000))
+    .then(()=> app.listen(5001))
     .then(()=> console.log("Connected"))
     .catch((err)=> console.log(err));
