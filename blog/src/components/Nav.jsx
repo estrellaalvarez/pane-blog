a5d6410beab9fb1ee265e84abdc41b0aaf654400
estrellalvarez <alvare89@uwm.edu>
2022-10-14 09:32:15 -0500

    Routes mapped out

---
+++ blog/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='navbar'>
+        <h3>Pane Blog</h3>
+        <button>Login</button>
+        <button>Sign Up</button>
+        </div>
+  )
+}
+
+export default Nav
