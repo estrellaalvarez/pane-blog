826bcabd2db6fe38d9f75775fed1b784bb295169
estrellalvarez <alvare89@uwm.edu>
2022-10-14 01:03:08 -0500

    Update and Delete Routes

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -24,4 +24,52 @@ export const addBlog = async(req, res, next) => {
         return console.log(err)
     }
     return res.status(200).json({blog})
+};
+
+export const updateBlog = async(req, res, next) => {
+    const {title, description} = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description
+        })
+    } catch(err) {
+        return console.log(err)
+    }
+    if(!blog) {
+        return res.status(500).json({message: "Coulndn't Update"})
+    }
+    return res.status(200).json({blog});
 }
+
+export const getById = async(req, res, next) => {
+    const id = req.params.id;
+    let blog;
+    try {
+        blog = Blog.findById(id);
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!blog) {
+        return res.status(404).json({message: "No Blog Here"})
+    }
+    return res.status(200).json({blog})
+};
+
+export const deleteBlog = async(req, res, next) => {
+    const id = req.params.id;
+
+    let blog;
+    try {
+        blog = await Blog.findByIdAndRemove(id)
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blog) {
+        return res.status(400).json({message: "Could not delete"})
+    } return res.status(200).json({message: "Blog Deleted"})
+}
+;
+
